b206dcfd60c92e6d51ecf8760f60b9e691ecdaa9
Kevin Loustau <contact@kevinloustau.fr>
2024-09-14 22:50:26 +0200

    auto mode + a minor

--- Scenes/Particle.gd
+++ Scenes/Particle.gd
@@ -5,8 +5,5 @@ extends Node3D
 @onready var audio_player := $AudioStreamPlayer
 @onready var ball := $CSGMesh3D
 
-func _ready():
-	audio_player.stream = files[randi() % files.size()]
-
 func play_sound():
 	audio_player.play()
--- Scenes/TE-Tombola.gd
+++ Scenes/TE-Tombola.gd
@@ -1,17 +1,42 @@
 extends Node3D
 
+@export var auto_modulation: bool = false
 @onready var mainShape = $MainShape
 @onready var area = $Area3D
 @onready var timer: Timer = $NotesEmitter/Timer
 var mainShapeSpeed : int = 0
 
+@onready var ui: Control = $UI
+
+func _ready() -> void:
+	if auto_modulation:
+		ui.visible = false
+		if auto_modulation:
+			play_random_anim()
+
+
+
+func play_random_anim():
+	var t = create_tween()
+	t.parallel().tween_property(self, "mainShapeSpeed", randf() * 100.0 , randf_range(1,10)) # slider speed 0 - 100
+	t.parallel().tween_method(_on_angle_value_changed, previous_angle_value, randf() , randf_range(1,10)) # slider angle 0 - 1
+	t.parallel().tween_method(_on_ball_amount_value_changed, previous_ball_amount_value, randf_range(0.1,4), randf_range(1,10)) # ball amount 0 - 100
+	t.parallel().tween_method(_on_gravity_value_changed, previous_gravity_value, randf(), randf_range(1,10)) # slider gravity 0 - 1
+	await t.finished
+	if auto_modulation: play_random_anim()
+
 func _process(delta):
 	mainShape.rotate_z( -delta * mainShapeSpeed * 0.025)
 
+var previous_gravity_value: = 0.0
 func _on_gravity_value_changed(value):
+	previous_gravity_value = value
 	area.gravity = value * 9.8
 
+
+var previous_angle_value: = 0.0
 func _on_angle_value_changed(value):
+	previous_angle_value = value
 	var value_lerped = lerp(0, 90, value)
 	for s in mainShape.get_children():
 		if s is Node3D:
@@ -20,11 +45,27 @@ func _on_angle_value_changed(value):
 func _on_rotation_speed_value_changed(value):
 	mainShapeSpeed = value
 
+var previous_ball_amount_value: = 0.0
 func _on_ball_amount_value_changed(value: float) -> void:
-	if value < 10:
+	if value <= 0.1:
 		timer.stop()
 	else:
-		print(value)
 		timer.start()
-		timer.wait_time = 100 / (value * 4)
-		print("timer.wait_time: " , 100 / value)
+		previous_ball_amount_value = 4.1 - (value * 4.0)
+		timer.wait_time = 4.1 - (value * 4.0)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if event.double_tap:
+			toggle_auto_mode()
+	elif event.is_action_pressed("toggle_auto_mode"):
+		toggle_auto_mode()
+
+func toggle_auto_mode():
+	ui.visible = !ui.visible
+	if ui.visible ==  false:
+		auto_modulation = true
+		play_random_anim()
+	else:
+		auto_modulation = false
